abbc0a592ff031b4c9ca985b1cff162520ed5c30
Pedro Henrique Pinheiro de Oliveira <pedrocaseis@hotmail.com>
2024-06-20 12:08:56 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,4 @@
+var preco = prompt(`Insira um preço ai`)
+var idade = prompt(`Digite a sua idade`)
+
+idade < 18 ? console.log(`Menor de idade, receba o desconto ${preco *= 0.9} ` ) : console.log(`Maior de idade, sem desconto pra vc, coroa`)
